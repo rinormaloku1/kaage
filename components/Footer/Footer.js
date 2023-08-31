779584e71d1875c04d796ad20c83024f46a215f9
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 18:31:26 +0200

    fix/footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -29,11 +29,11 @@ export default function Footer(props) {
   return (
     <footer className={footerClasses}>
       <div className={classes.container}>
-        <div className={classes.left}>
+        {/* <div className={classes.left}>
           <List className={classes.list}>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/?ref=njsmk-footer"
+                href="https://arml.digital"
                 className={classes.block}
                 target="_blank"
               >
@@ -42,7 +42,7 @@ export default function Footer(props) {
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/presentation?ref=njsmk-footer"
+                href="https://arml.digital"
                 className={classes.block}
                 target="_blank"
               >
@@ -51,7 +51,7 @@ export default function Footer(props) {
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="http://blog.creative-tim.com/?ref=njsmk-footer"
+                href="https://arml.digital"
                 className={classes.block}
                 target="_blank"
               >
@@ -60,7 +60,7 @@ export default function Footer(props) {
             </ListItem>
             <ListItem className={classes.inlineBlock}>
               <a
-                href="https://www.creative-tim.com/license?ref=njsmk-footer"
+                href="https://arml.digital"
                 className={classes.block}
                 target="_blank"
               >
@@ -68,16 +68,16 @@ export default function Footer(props) {
               </a>
             </ListItem>
           </List>
-        </div>
+        </div> */}
         <div className={classes.right}>
           &copy; {1900 + new Date().getYear()} , made with{" "}
           <Favorite className={classes.icon} /> by{" "}
           <a
-            href="https://www.creative-tim.com?ref=njsmk-footer"
+            href="https://arml.digital"
             className={aClasses}
             target="_blank"
           >
-            Creative Tim
+            ARML AGENCY
           </a>{" "}
           for a better web.
         </div>
