5e489062c2f99b196b8878060ef399b255bf1c8c
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 16:43:05 +0200

    fix/productS

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -21,13 +21,9 @@ export default function ProductSection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Let{"'"}s talk product</h2>
+          <h2 className={classes.title}>Let{"'"}s talk products</h2>
           <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
+            Shkruaj diqka rreth produkteve
           </h5>
         </GridItem>
       </GridContainer>
@@ -35,9 +31,9 @@ export default function ProductSection() {
         <GridContainer>
           <GridItem xs={12} sm={12} md={4}>
             <InfoArea
-              title="Free Chat"
-              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={Chat}
+              title="Eggs"
+              description="Shkruaj per vezet"
+              icon={Egg}
               iconColor="info"
               vertical
             />
