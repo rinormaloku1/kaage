da61fb3b3783be52f556c74719e2a850a7b4bf57
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 19:08:26 +0200

    fix/chat

--- pages/landing.js
+++ pages/landing.js
@@ -22,6 +22,7 @@ import ProductSection from "/pages-sections/LandingPage-Sections/ProductSection.
 import SectionCarousel from "/pages-sections/LandingPage-Sections/SliderSection.js";
 import TeamSection from "/pages-sections/LandingPage-Sections/TeamSection.js";
 import WorkSection from "/pages-sections/LandingPage-Sections/WorkSection.js";
+import Chat from "/pages-sections/LandingPage-Sections/chatbot.js";
 
 const dashboardRoutes = [];
 
@@ -73,6 +74,7 @@ export default function LandingPage(props) {
           <SectionCarousel />
           {/* <TeamSection /> */}
           <WorkSection />
+          <Chat/>
         </div>
       </div>
       <Footer />
