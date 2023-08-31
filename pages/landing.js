89305060208d91caf56b1ee6d34cb2a40aeb95ee
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 16:28:12 +0200

    fix/landing

--- pages/landing.js
+++ pages/landing.js
@@ -35,7 +35,7 @@ export default function LandingPage(props) {
         color="transparent"
         routes={dashboardRoutes}
         brand="Kaage"
-        rightLinks={<HeaderLinks />}
+        rightLinks= "#"
         fixed
         changeColorOnScroll={{
           height: 400,
@@ -47,18 +47,15 @@ export default function LandingPage(props) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>KAAGE - Vezë të freskëta Katuni.</h1>
               <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
+                Shkruaj diqka
               </h4>
               <br />
               <Button
                 color="danger"
                 size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                href="#"
                 target="_blank"
                 rel="noopener noreferrer"
               >
