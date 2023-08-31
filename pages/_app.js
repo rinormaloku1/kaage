9fdca262077c38d45407f78b0aaccc07d24d3ca2
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 18:51:47 +0200

    fix/title

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title><strong>Kaage</strong></title>
+          <title><b>Kaage</b></title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
