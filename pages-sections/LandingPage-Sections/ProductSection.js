ea920222bfc98c13acf94899f04af217a36340cb
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 16:56:40 +0200

    fix/productS

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -1,216 +1,191 @@
+// import React from "react";
+// // @material-ui/core components
+// import { makeStyles } from "@material-ui/core/styles";
+
+// // @material-ui/icons
+// import Chat from "@material-ui/icons/Chat";
+// import VerifiedUser from "@material-ui/icons/VerifiedUser";
+// import Fingerprint from "@material-ui/icons/Fingerprint";
+// // core components
+// import GridContainer from "/components/Grid/GridContainer.js";
+// import GridItem from "/components/Grid/GridItem.js";
+// import InfoArea from "/components/InfoArea/InfoArea.js";
+
+// import styles from "/styles/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js";
+
+// const useStyles = makeStyles(styles);
+
+// export default function ProductSection() {
+//   const classes = useStyles();
+//   return (
+//     <div className={classes.section}>
+//       <GridContainer justify="center">
+//         <GridItem xs={12} sm={12} md={8}>
+//           <h2 className={classes.title}>Let{"'"}s talk products</h2>
+//           <h5 className={classes.description}>
+//             Shkruaj diqka rreth produkteve
+//           </h5>
+//         </GridItem>
+//       </GridContainer>
+//       <div>
+//         <GridContainer>
+//           <GridItem xs={12} sm={12} md={4}>
+//             <InfoArea
+//               title="Eggs"
+//               description="Shkruaj per vezet"
+//               icon={Chat}
+//               iconColor="info"
+//               vertical
+//             />
+//           </GridItem>
+//           <GridItem xs={12} sm={12} md={4}>
+//             <InfoArea
+//               title="Verified Users"
+//               description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+//               icon={VerifiedUser}
+//               iconColor="success"
+//               vertical
+//             />
+//           </GridItem>
+//           <GridItem xs={12} sm={12} md={4}>
+//             <InfoArea
+//               title="Fingerprint"
+//               description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
+//               icon={Fingerprint}
+//               iconColor="danger"
+//               vertical
+//             />
+//           </GridItem>
+//         </GridContainer>
+//       </div>
+//     </div>
+//   );
+// }
 import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
-// @material-ui/icons
-import Chat from "@material-ui/icons/Chat";
-import VerifiedUser from "@material-ui/icons/VerifiedUser";
-import Fingerprint from "@material-ui/icons/Fingerprint";
 // core components
 import GridContainer from "/components/Grid/GridContainer.js";
 import GridItem from "/components/Grid/GridItem.js";
-import InfoArea from "/components/InfoArea/InfoArea.js";
 
-import styles from "/styles/jss/nextjs-material-kit/pages/landingPageSections/productStyle.js";
+import styles from "/styles/jss/nextjs-material-kit/components/navPillsStyle.js";
 
 const useStyles = makeStyles(styles);
 
-export default function ProductSection() {
+export default function NavPills(props) {
+  const [active, setActive] = React.useState(props.active);
+  const handleChange = (event, active) => {
+    setActive(active);
+  };
+  const handleChangeIndex = (index) => {
+    setActive(index);
+  };
   const classes = useStyles();
-  return (
-    <div className={classes.section}>
-      <GridContainer justify="center">
-        <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Let{"'"}s talk products</h2>
-          <h5 className={classes.description}>
-            Shkruaj diqka rreth produkteve
-          </h5>
-        </GridItem>
-      </GridContainer>
-      {/* <div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={4}>
-            <InfoArea
-              title="Eggs"
-              description="Shkruaj per vezet"
-              icon={Chat}
-              iconColor="info"
-              vertical
-            />
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <InfoArea
-              title="Verified Users"
-              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={VerifiedUser}
-              iconColor="success"
-              vertical
-            />
-          </GridItem>
-          <GridItem xs={12} sm={12} md={4}>
-            <InfoArea
-              title="Fingerprint"
-              description="Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough."
-              icon={Fingerprint}
-              iconColor="danger"
-              vertical
-            />
-          </GridItem>
-        </GridContainer>
-      </div> */}
-      <div>
-      <GridContainer>
-            <GridItem xs={12} sm={12} md={8} lg={6}>
-              <NavPills
-                color="primary"
-                tabs={[
-                  {
-                    tabButton: "Dashboard",
-                    tabIcon: Dashboard,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  },
-                  {
-                    tabButton: "Schedule",
-                    tabIcon: Schedule,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Efficiently unleash cross-media information without
-                          cross-media value. Quickly maximize timely
-                          deliverables for real-time schemas.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically maintain clicks-and-mortar solutions
-                          without functional solutions. Dramatically visualize
-                          customer directed convergence without revolutionary
-                          ROI. Collaboratively administrate empowered markets
-                          via plug-and-play networks. Dynamically procrastinate
-                          B2C users after installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  },
-                  {
-                    tabButton: "Tasks",
-                    tabIcon: List,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  }
-                ]}
-              />
-            </GridItem>
-            <GridItem xs={12} sm={12} md={12} lg={6}>
-              <NavPills
-                color="rose"
-                horizontal={{
-                  tabsGrid: { xs: 12, sm: 4, md: 4 },
-                  contentGrid: { xs: 12, sm: 8, md: 8 }
-                }}
-                tabs={[
-                  {
-                    tabButton: "Dashboard",
-                    tabIcon: Dashboard,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  },
-                  {
-                    tabButton: "Schedule",
-                    tabIcon: Schedule,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Efficiently unleash cross-media information without
-                          cross-media value. Quickly maximize timely
-                          deliverables for real-time schemas.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically maintain clicks-and-mortar solutions
-                          without functional solutions. Dramatically visualize
-                          customer directed convergence without revolutionary
-                          ROI. Collaboratively administrate empowered markets
-                          via plug-and-play networks. Dynamically procrastinate
-                          B2C users after installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  }
-                ]}
-              />
-            </GridItem>
-          </GridContainer>
-      </div>
+  const { tabs, color, horizontal, alignCenter } = props;
+  const flexContainerClasses = classNames({
+    [classes.flexContainer]: true,
+    [classes.horizontalDisplay]: horizontal !== undefined
+  });
+  const tabButtons = (
+    <Tabs
+      classes={{
+        root: classes.root,
+        fixed: classes.fixed,
+        flexContainer: flexContainerClasses,
+        indicator: classes.displayNone
+      }}
+      value={active}
+      onChange={handleChange}
+      centered={alignCenter}
+    >
+      {tabs.map((prop, key) => {
+        var icon = {};
+        if (prop.tabIcon !== undefined) {
+          icon["icon"] = <prop.tabIcon className={classes.tabIcon} />;
+        }
+        const pillsClasses = classNames({
+          [classes.pills]: true,
+          [classes.horizontalPills]: horizontal !== undefined,
+          [classes.pillsWithIcons]: prop.tabIcon !== undefined
+        });
+        return (
+          <Tab
+            label={prop.tabButton}
+            key={key}
+            {...icon}
+            classes={{
+              root: pillsClasses,
+              selected: classes[color],
+              wrapper: classes.tabWrapper
+            }}
+          />
+        );
+      })}
+    </Tabs>
+  );
+  const tabContent = (
+    <div className={classes.contentWrapper}>
+      {tabs.map((prop, key) => {
+        if (key !== active) {
+          return null;
+        } else {
+          return (
+            <div className={classes.tabContent} key={key}>
+              {prop.tabContent}
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+  return horizontal !== undefined ? (
+    <GridContainer>
+      <GridItem {...horizontal.tabsGrid}>{tabButtons}</GridItem>
+      <GridItem {...horizontal.contentGrid}>{tabContent}</GridItem>
+    </GridContainer>
+  ) : (
+    <div>
+      {tabButtons}
+      {tabContent}
     </div>
   );
 }
+
+NavPills.defaultProps = {
+  active: 0,
+  color: "primary"
+};
+
+NavPills.propTypes = {
+  // index of the default active pill
+  active: PropTypes.number,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      tabButton: PropTypes.string,
+      tabIcon: PropTypes.object,
+      tabContent: PropTypes.node
+    })
+  ).isRequired,
+  color: PropTypes.oneOf([
+    "primary",
+    "warning",
+    "danger",
+    "success",
+    "info",
+    "rose"
+  ]),
+  horizontal: PropTypes.shape({
+    tabsGrid: PropTypes.object,
+    contentGrid: PropTypes.object
+  }),
+  alignCenter: PropTypes.bool
+};
+
