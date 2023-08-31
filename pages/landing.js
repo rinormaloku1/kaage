03ed1b403baa12f69c11fa8775f1e5fbdcd1605d
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 19:19:35 +0200

    fix/all

--- pages/landing.js
+++ pages/landing.js
@@ -74,7 +74,6 @@ export default function LandingPage(props) {
           <SectionCarousel />
           {/* <TeamSection /> */}
           <WorkSection />
-          <Chat/>
         </div>
       </div>
       <Footer />
