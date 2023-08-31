b01fe17896ee7763ffcd5209953c9625f94d7d50
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 16:46:47 +0200

    fix/landing

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -33,8 +33,6 @@ export default function ProductSection() {
             <InfoArea
               title="Eggs"
               description="Shkruaj per vezet"
-              icon={Egg}
-              iconColor="info"
               vertical
             />
           </GridItem>
