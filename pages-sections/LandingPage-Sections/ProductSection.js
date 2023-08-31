72a6a0115873559bc883d1a93fd1c7d9dffeb0d7
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 17:08:48 +0200

    fix/productS

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -100,93 +100,6 @@ export default function SectionPills() {
             </h3>
           </div>
           <GridContainer>
-            <GridItem xs={12} sm={12} md={8} lg={6}>
-              <NavPills
-                color="primary"
-                tabs={[
-                  {
-                    tabButton: "Dashboard",
-                    tabIcon: Dashboard,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  },
-                  {
-                    tabButton: "Schedule",
-                    tabIcon: Schedule,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Efficiently unleash cross-media information without
-                          cross-media value. Quickly maximize timely
-                          deliverables for real-time schemas.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically maintain clicks-and-mortar solutions
-                          without functional solutions. Dramatically visualize
-                          customer directed convergence without revolutionary
-                          ROI. Collaboratively administrate empowered markets
-                          via plug-and-play networks. Dynamically procrastinate
-                          B2C users after installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  },
-                  {
-                    tabButton: "Tasks",
-                    tabIcon: List,
-                    tabContent: (
-                      <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                      </span>
-                    )
-                  }
-                ]}
-              />
-            </GridItem>
             <GridItem xs={12} sm={12} md={12} lg={6}>
               <NavPills
                 color="rose"
@@ -196,7 +109,7 @@ export default function SectionPills() {
                 }}
                 tabs={[
                   {
-                    tabButton: "Dashboard",
+                    tabButton: "Eggs",
                     tabIcon: Dashboard,
                     tabContent: (
                       <span>
@@ -225,7 +138,7 @@ export default function SectionPills() {
                     )
                   },
                   {
-                    tabButton: "Schedule",
+                    tabButton: "Trees",
                     tabIcon: Schedule,
                     tabContent: (
                       <span>
