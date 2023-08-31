2f4977861fdfa89d83fad19d4d644857464f167a
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 17:25:57 +0200

    fix/productS

--- styles/jss/nextjs-material-kit/pages/componentsSections/pillsStyle.js
+++ styles/jss/nextjs-material-kit/pages/componentsSections/pillsStyle.js
@@ -3,20 +3,21 @@ import { container, title } from "/styles/jss/nextjs-material-kit.js";
 const pillsStyle = {
   section: {
     padding: "70px 0",
-    textAlign: "center"
   },
   title: {
     ...title,
     marginBottom: "1rem",
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
+    textAlign: "center"
   },
   container,
   description: {
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
+    color: "#999"
   }
 };
 
