9788f4607c492faa96a5a82d0e8baaaa383f0a78
rinormaloku1 <142502517+rinormaloku1@users.noreply.github.com>
2023-08-31 17:17:28 +0200

    fix/productS

--- pages-sections/LandingPage-Sections/ProductSection.js
+++ pages-sections/LandingPage-Sections/ProductSection.js
@@ -112,48 +112,18 @@ export default function SectionPills() {
                     tabButton: "Eggs",
                     tabContent: (
                       <span>
-                        <p>
-                          Collaboratively administrate empowered markets via
-                          plug-and-play networks. Dynamically procrastinate B2C
-                          users after installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically visualize customer directed convergence
-                          without revolutionary ROI. Collaboratively
-                          administrate empowered markets via plug-and-play
-                          networks. Dynamically procrastinate B2C users after
-                          installed base benefits.
+                        <p className={classes.title}>
+                        Shkruaj diqka per vezet
                         </p>
                       </span>
                     )
                   },
                   {
                     tabButton: "Trees",
-                    tabIcon: Schedule,
                     tabContent: (
                       <span>
-                        <p>
-                          Efficiently unleash cross-media information without
-                          cross-media value. Quickly maximize timely
-                          deliverables for real-time schemas.
-                        </p>
-                        <br />
-                        <p>
-                          Dramatically maintain clicks-and-mortar solutions
-                          without functional solutions. Dramatically visualize
-                          customer directed convergence without revolutionary
-                          ROI. Collaboratively administrate empowered markets
-                          via plug-and-play networks. Dynamically procrastinate
-                          B2C users after installed base benefits.
+                        <p className={classes.title}>
+                          Shkruaj diqka per pemet
                         </p>
                       </span>
                     )
